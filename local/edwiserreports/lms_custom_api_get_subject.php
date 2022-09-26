1041bc54c0046ec11afcfbe584866153ec4bb1cb
nobita12377 <88657553+nobita12377@users.noreply.github.com>
2022-09-26 23:08:34 +0700

    local_edwiserreports fix get all course in a semester for block course progress

--- local/edwiserreports/lms_custom_api_get_subject.php
+++ local/edwiserreports/lms_custom_api_get_subject.php
@@ -3,18 +3,25 @@
 require_once("../../config.php");
 
 $subjectManageId = required_param('subjectManageId', PARAM_INT);
+$isGetAll = optional_param('action', '', PARAM_TEXT);
 
-$sql = "SELECT id FROM `". $CFG->prefix ."course_categories` WHERE path LIKE '%/". $subjectManageId ."/%'";
-$childCategories = $DB->get_records_sql($sql);
-
-$condition = '';
-foreach ($childCategories as $category) {
-    $condition .= $category->id . ',';
+$courses = null;
+if ($isGetAll === 'getall') {
+    $sql = "SELECT id FROM `". $CFG->prefix ."course_categories` WHERE path LIKE '%/". $subjectManageId ."/%'";
+    $childCategories = $DB->get_records_sql($sql);
+    
+    $condition = '';
+    foreach ($childCategories as $category) {
+        $condition .= $category->id . ',';
+    }
+    
+    $condition = rtrim($condition, ",");
+    $sql = 'SELECT id, fullname FROM `'. $CFG->prefix .'course` WHERE category IN ('. $condition .')';
+    $courses = $DB->get_records_sql($sql);
+} else {
+    $courses = get_courses($subjectManageId);
 }
 
-$condition = rtrim($condition, ",");
-$sql = 'SELECT id, fullname FROM `'. $CFG->prefix .'course` WHERE category IN ('. $condition .')';
-$courses = $DB->get_records_sql($sql);
 
 $data = [];
 foreach ($courses as $course) {
